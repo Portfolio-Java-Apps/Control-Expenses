c2852653fd63b400148330bfcecc560535c70350
Gonçalo Castro Lopes <57234789+gclopes1978@users.noreply.github.com>
2021-08-21 16:16:25 +0100

    Create Springboot Project
    Create the project according the Development requirements.

---
+++ src/test/java/dev/gclopes/ControlExpenses/ControlExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gclopes.ControlExpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
